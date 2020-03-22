b4c76b59647664aeb961ebc8be9679c7ea24dbad
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-03-22 20:30:14 +0300

    feat: add solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,27 @@
-module.exports = function repeater(/*str, options*/) {
-    throw 'Not implemented';      
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (options.repeatTimes === undefined) {
+      options.repeatTimes = 1;
+    }
+
+    if (options.separator === undefined) {
+      options.separator = "+";
+    }
+
+    if (options.addition === undefined) {
+      options.addition = "";
+    }
+
+    if (options.additionRepeatTimes === undefined) {
+      options.additionRepeatTimes = 1;
+    }
+
+    if (options.additionSeparator === undefined) {
+      options.additionSeparator = "|";
+    }
+
+    let substr = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes).slice(0, -options.additionSeparator.length);
+    let answer = (str + substr + options.separator).repeat(options.repeatTimes).slice (0, -options.separator.length);
+    return answer;
+ 
 };
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,25 @@
-module.exports = function transform(/*arr*/) {
-    throw 'Not implemented';
-    /*let answer = [];
-    if (!Array.isArray(arr)) {
-        throw new Error("Not an array");
-    } else if (arr.length == 0) {
-        return arr;
-    } else {
-
-        for( let i = 0; i < arr.length; i++) {
-            if (arr[i] === "--double-next" && i!=arr.length-1) {
-                answer.push(arr[i+1]);
-            } else if (arr[i] === "--double-prev") {
-                answer.push(arr[i-1]);
-            } else if (arr[i] === "--discard-prev") {
-                answer.pop();
-            } else if (arr[i] ==="--discard-next") {
-                i+=1;
-            } else answer.push(arr[i]);
-        }
-        return answer;
-    }*/
+module.exports = function transform(arr) {
     
+    if (!Array.isArray(arr)) {
+        throw new Error;
+    }
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] ==="--discard-next") {
+            arr.splice([arr[i + 1], 1]);
+        } else if (arr[i] === "--discard-prev") {
+            if (i == arr.length -1) {
+                arr.splice(arr[i], 1);
+            } else {
+                arr.splice(arr[i-1], 1);
+            }
+        } else if (arr[i] === "--double-next") {
+            arr.splice(arr[i + 1], 0, arr[i]);
+        } else if (arr[i] === "--double-prev") {
+            arr.splice(arr[i-1], 0, arr[i]);
+        } else if (arr.length < 1) {
+            return arr;
+        }
+    } return arr;
     
 };
